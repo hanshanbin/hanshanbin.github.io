3e3d347fd8b786c085f73a079d1ee335d43bd29c
HansHanbin <hans.hanbin.lee@gmail.com>
2022-10-23 23:46:55 +0900

    about 수정정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <Routes>
       <Route></Route>
-      <Route path="/design" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
     </Routes>
   );
